eec4d31a84f02e2650ab1edbd4a5d1a0ade9f994
Haghl <wdg980813@gmail.com>
2023-02-15 16:48:49 +0900

    build: 빌드테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">나의 블로그!</a>
+          어서와 <a href="https://nextjs.org">나의 블로그!</a>
         </h1>
 
         <p className={styles.description}>
