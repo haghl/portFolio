df12ecffdeb53c3ce0e969945cc91353734a2b1a
DongGyu Won <wdg980813@gmail.com>
2023-03-22 01:43:13 +0900

    feat: 파비콘 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Favicon from '@/components/common/Favicon'
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="ko">
-        <Head />
+        <Head>
+          <Favicon />
+        </Head>
 
         <body>
           <Main />
