1e426a8f1030c0a3b5fb55e38273918ef17fac97
DongGyu Won <wdg980813@gmail.com>
2023-04-03 19:08:54 +0900

    build: 빌드용 수정

--- src/pages/blog/[pageid].tsx
+++ src/pages/blog/[pageid].tsx
@@ -1,60 +1,60 @@
-import type { GetStaticPaths, GetStaticProps } from 'next'
-// import type { GetServerSideProps } from 'next'
-import { useRouter } from 'next/router'
-import type { ExtendedRecordMap } from 'notion-types'
-import { getPageTitle } from 'notion-utils'
-import PageHead from '@/components/common/PageHead'
-import NotionRender from '@/components/common/NotionRender'
-import NotionSkeleton from '@/components/views/NotionSkeleton'
-import { getCachedDatabaseItems } from '@/notion/utils/getCachedDatabaseItems'
-import { getPageContent } from '@/notion/notion'
+// import type { GetStaticPaths, GetStaticProps } from 'next'
+// // import type { GetServerSideProps } from 'next'
+// import { useRouter } from 'next/router'
+// import type { ExtendedRecordMap } from 'notion-types'
+// import { getPageTitle } from 'notion-utils'
+// import PageHead from '@/components/common/PageHead'
+// import NotionRender from '@/components/common/NotionRender'
+// import NotionSkeleton from '@/components/views/NotionSkeleton'
+// import { getCachedDatabaseItems } from '@/notion/utils/getCachedDatabaseItems'
+// import { getPageContent } from '@/notion/notion'
 
-interface IDetailsPage {
-  recordMap: ExtendedRecordMap
-}
+// interface IDetailsPage {
+//   recordMap: ExtendedRecordMap
+// }
 
-const DetailsPage = ({ recordMap }: IDetailsPage) => {
-  const { isFallback } = useRouter()
-  const pageTitle = recordMap ? getPageTitle(recordMap) : ''
+// const DetailsPage = ({ recordMap }: IDetailsPage) => {
+//   const { isFallback } = useRouter()
+//   const pageTitle = recordMap ? getPageTitle(recordMap) : ''
 
-  if (isFallback) return <NotionSkeleton />
+//   if (isFallback) return <NotionSkeleton />
 
-  return (
-    <>
-      <PageHead subTitle={pageTitle} />
-      <NotionRender recordMap={recordMap} />
-    </>
-  )
-}
+//   return (
+//     <>
+//       <PageHead subTitle={pageTitle} />
+//       <NotionRender recordMap={recordMap} />
+//     </>
+//   )
+// }
 
-export const getStaticProps: GetStaticProps<IDetailsPage> = async ({ params }) => {
-  // export const getServerSideProps: GetServerSideProps<IDetailsPage> = async ({ params }) => {
-  const id = params?.id
-  try {
-    if (!id) throw Error('id is not defined')
-    const recordMap = await getPageContent(id.toString())
-    return {
-      props: { recordMap },
-      revalidate: 1,
-    }
-  } catch (e) {
-    console.error(e)
-    return { notFound: true }
-  }
-}
+// export const getStaticProps: GetStaticProps<IDetailsPage> = async ({ params }) => {
+//   // export const getServerSideProps: GetServerSideProps<IDetailsPage> = async ({ params }) => {
+//   const id = params?.id
+//   try {
+//     if (!id) throw Error('id is not defined')
+//     const recordMap = await getPageContent(id.toString())
+//     return {
+//       props: { recordMap },
+//       revalidate: 1,
+//     }
+//   } catch (e) {
+//     console.error(e)
+//     return { notFound: true }
+//   }
+// }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const databaseId = process.env.NOTION_DATABASE_ID
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const databaseId = process.env.NOTION_DATABASE_ID
 
-  if (!databaseId) throw new Error('DATABASE_ID is not defined')
-  const databaseItems = await getCachedDatabaseItems(databaseId)
-  const paths = databaseItems.map(({ id }) => ({
-    params: { id },
-  }))
-  return {
-    paths,
-    fallback: true,
-  }
-}
+//   if (!databaseId) throw new Error('DATABASE_ID is not defined')
+//   const databaseItems = await getCachedDatabaseItems(databaseId)
+//   const paths = databaseItems.map(({ id }) => ({
+//     params: { id },
+//   }))
+//   return {
+//     paths,
+//     fallback: true,
+//   }
+// }
 
-export default DetailsPage
+// export default DetailsPage
